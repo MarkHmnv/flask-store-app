ba3a3e7d232bf77b4137b6ecbc5fbef41f6571f4
MarkHmnv <skrytyjinkognito54@gmail.com>
2023-11-01 20:53:18 +0200

    Add JWT authentication to API views and adjust identifiers
    Introduced JWT authentication to all API views, ensuring a higher level of security for all data transactions. All routes' 'id' parameters are now expected to be integers for better system compatibility and performance. Added user model and schema to allow individual user-based interactions. Implemented changes provide improved robustness, regulation-compliant data handling and user-specific customization possibilities.

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -2,3 +2,4 @@ from .store import StoreModel
 from .item import ItemModel
 from .tag import TagModel
 from .item_tags import ItemTags
+from .user import UserModel
--- app/views/tag.py
+++ app/views/tag.py
@@ -1,4 +1,5 @@
 from flask.views import MethodView
+from flask_jwt_extended import jwt_required
 from flask_smorest import Blueprint, abort
 from sqlalchemy.exc import SQLAlchemyError
 
@@ -9,13 +10,14 @@ from ..schemas import TagSchema
 blp = Blueprint('tags', __name__, description='Tags API')
 
 
-@blp.route('/stores/<string:id>/tags')
+@blp.route('/stores/<int:id>/tags')
 class TagInStoreView(MethodView):
     @blp.response(200, TagSchema(many=True))
     def get(self, id):
         store = StoreModel.query.get_or_404(id)
         return store.tags.all()
 
+    @jwt_required(fresh=True)
     @blp.arguments(TagSchema)
     @blp.response(200, TagSchema)
     def post(self, data, id):
@@ -32,13 +34,14 @@ class TagInStoreView(MethodView):
         return tag
 
 
-@blp.route('/tags/<string:id>')
+@blp.route('/tags/<int:id>')
 class TagView(MethodView):
     @blp.response(200, TagSchema)
     def get(self, id):
         tag = TagModel.query.get_or_404(id)
         return tag
 
+    @jwt_required(fresh=True)
     @blp.response(204)
     @blp.alt_response(404, description='Tag not found')
     @blp.alt_response(400, description='Returned if the tag is assigned to one or more items')
@@ -51,8 +54,9 @@ class TagView(MethodView):
         abort(400, message='Tag cannot be deleted because it is assigned to one or more items')
 
 
-@blp.route('/items/<string:item_id>/tags/<string:tag_id>')
+@blp.route('/items/<int:item_id>/tags/<int:tag_id>')
 class LinkTagsToItemView(MethodView):
+    @jwt_required(fresh=True)
     @blp.response(201, TagSchema)
     def post(self, item_id, tag_id):
         item = ItemModel.query.get_or_404(item_id)
@@ -67,6 +71,7 @@ class LinkTagsToItemView(MethodView):
 
         return tag
 
+    @jwt_required(fresh=True)
     @blp.response(204)
     def delete(self, item_id, tag_id):
         item = ItemModel.query.get_or_404(item_id)
